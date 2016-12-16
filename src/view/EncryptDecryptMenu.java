b13e3b9fd55cd9f1d7fcbde29ae9d9599d17101d
Cliff Panos <Cliff@lawn-128-61-1-206.lawn.gatech.edu>
2016-12-16 13:11:02 -0500

    Cliff changed E/D Menu

--- src/view/EncryptDecryptMenu.java
+++ src/view/EncryptDecryptMenu.java
@@ -1,11 +1,20 @@
 package view;
 
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
 
 public class EncryptDecryptMenu {
 
     private static VBox menu = new VBox(10);
 
+    public EncryptDecryptMenu() {
+        //change the height and width and arc arguments to be a percentage of something
+        Rectangle whiteBackground = new Rectangle(75, 25,
+            Color.web("#F7F7F7", 0.72));
+        whiteBackground.setArcWidth(7);
+        whiteBackground.setArcHeight(7);
+    }
 
     public static VBox getRoodNode() {
         return menu;
